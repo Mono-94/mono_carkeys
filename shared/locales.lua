cca12fd1fb3fbe82ec699f08ecbb3ba0ba5b3da4
MONO <68543161+Mono-94@users.noreply.github.com>
2023-12-01 13:59:07 +0100

    Update locales.lua

--- shared/locales.lua
+++ shared/locales.lua
@@ -11,33 +11,39 @@ Carkey.Locales = {
     ['ES'] = {
         VehicleOpen = 'Vehículo abierto',
         VehicleClose = 'Vehículo cerrado',
-        DoorsVehicleKeyBind = 'Abrir/Cerrar vehículo'
+        DoorsVehicleKeyBind = 'Abrir/Cerrar vehículo',
+         PlateMetadata = 'Matrícula: **%s**',
     },
     ['EN'] = {
         VehicleOpen = 'Vehicle open',
         VehicleClose = 'Vehicle closed',
         DoorsVehicleKeyBind = 'Open/Close vehicle',
+        PlateMetadata = 'License plate: **%s**',
     },
     ['PL'] = {
         VehicleOpen = 'Pojazd otwarty',
         VehicleClose = 'Pojazd zamknięty',
         DoorsVehicleKeyBind = 'Otwórz/Zamknij pojazd',
+        PlateMetadata = 'Numer rejestracyjny: **%s**',
 
     },
     ['FR'] = {
         VehicleOpen = 'Véhicule ouvert',
         VehicleClose = 'Véhicule fermé',
         DoorsVehicleKeyBind = 'Ouvrir/Fermer le véhicule',
+        PlateMetadata = 'Plaque d\'immatriculation : **%s**',
     },
     ['IT'] = {
         VehicleOpen = 'Veicolo aperto',
         VehicleClose = 'Veicolo chiuso',
         DoorsVehicleKeyBind = 'Apri/chiudi il veicolo',
+        PlateMetadata = 'Targa: **%s**',
 
     },
     ['PT'] = {
         VehicleOpen = 'Veículo aberto',
         VehicleClose = 'Veículo fechado',
         DoorsVehicleKeyBind = 'Abrir/Fechar veículo',
+        PlateMetadata = 'Placa: **%s**',
     },
 }
